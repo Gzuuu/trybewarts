086a24d1d4bc5223298adf67f311a65884334c1c
Henrique Paschoal <hfraia@yahoo.com.br>
2022-10-14 18:28:13 -0300

    Requisito 20 feito

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const email = document.getElementById('email');
 const senha = document.getElementById('password');
 const check = document.getElementById('agreement');
 const submit = document.getElementById('submit-btn');
+const tamanho = document.getElementById('textarea');
+let contador = tamanho.value.length;
+document.getElementById('counter').innerText = 500;
+
+function muda() {
+  contador = tamanho.value.length;
+  document.getElementById('counter').innerText = (500 - contador);
+}
+
+document.getElementById('textarea').addEventListener('keyup', muda);
 
 //  Função entrar
 function entrar() {
